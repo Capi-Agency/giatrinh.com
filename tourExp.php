f795076a8bdf47df1d4c78664d19659ef1c48ca3
ducnguyen1002 <ducduc1022000@gmail.com>
2023-08-08 16:25:37 +0700

    update images & spacing

--- tourExp.php
+++ tourExp.php
@@ -21,20 +21,7 @@
                             <div class="">></div>
                         </div>
                         <div class="col-auto">
-                            <div class="">Miền Trung</div>
-                        </div>
-                        <div class="col-auto">
-                            <div class="">></div>
-                        </div>
-                        <div class="col-auto">
-                            <div class="text-dark-1">Nha Trang</div>
-                        </div>
-                        <div class="col-auto">
-                            <div class="">></div>
-                        </div>
-                        <div class="col-auto">
-                            <div class="text-dark-1">Tour Hà Nội - Cam Ranh - Ninh Thuận - Nha Trang - Hà Nội ( 4 Ngày 3 đêm) Bay
-                                Vietnam airlines, Thứ 5,6,7 hàng tuần ) Tháng 5,6,7)</div>
+                            <div class="">Kinh nghiệm du lịch</div>
                         </div>
                     </div>
                 </div>
@@ -50,37 +37,41 @@
                 <div class="col-xl-9">
                     <div class="row y-gap-30">
                         <div class="col-xl-8">
-                            <img src="./img/blog/1.png" alt="">
-                            <div class="">
-                                <a class="text-18 fw-700 ">5 làng nghề truyền thống nổi tiếng ở miền Bắc</a>
+                            <div class="row y-gap-40 justify-center">
+                                <img class="col-12 rounded-8" src="./img/blog/1.png" alt="">
+                            </div>
+                            <div class="pt-30">
+                                <a href="./tourPost.php" class="text-18 fw-700">5 làng nghề truyền thống nổi tiếng ở miền Bắc</a>
                                 <p>
                                     Làng gốm Bát Tràng Bát Tràng là điểm đến không hề xa lạ đối với các bạn trẻ ưa thích nét văn hóa truyền thống của làng nghề gốm sứ V...
                                 </p>
                             </div>
                         </div>
                         <div class="col-xl-4">
-                            <img src="./img/blog/1.png" alt="">
-                            <div class="sidebar__item pt-30">
-                                <a class="text-18 fw-700 ">Chinh phục đỉnh núi Tà Chì Nhù, Yên Bái - 'thiên đường nơi hạ giới'</a>
-                                <div class="y-gap-5 border-bottom-light mt-10">
-                                    <a class="d-flex items-center justify-between text-dark-1" href="#">
+                            <div class="row justify-center">
+                                <img class="col-12 rounded-8" src="./img/blog/1.png" alt="">
+                            </div>
+                            <div class="sidebar__item pt-10 text-12">
+                                <a class="text-14 fw-700 ">Chinh phục đỉnh núi Tà Chì Nhù, Yên Bái - 'thiên đường nơi hạ giới'</a>
+                                <div class="y-gap-5 border-bottom-light py-10 mt-10">
+                                    <a href="./tourPost.php" class="d-flex items-center justify-between text-dark-1" href="#">
                                         Du lịch Bắc Giang, tuyệt đối không được bỏ qua 5 điểm đến 'cực hot'
                                     </a>
                                 </div>
-                                <div class="y-gap-5 border-bottom-light">
-                                    <a class="d-flex items-center justify-between text-dark-1" href="#">
+                                <div class="y-gap-5 border-bottom-light py-10 mt-10">
+                                    <a href="./tourPost.php" class="d-flex items-center justify-between text-dark-1" href="#">
                                         Du lịch Bắc Giang, tuyệt đối không được bỏ qua 5 điểm đến 'cực hot'
                                     </a>
                                 </div>
-                                <div class="y-gap-5 border-bottom-light">
-                                    <a class="d-flex items-center justify-between text-dark-1" href="#">
+                                <div class="y-gap-5 border-bottom-light py-10 mt-10">
+                                    <a href="./tourPost.php" class="d-flex items-center justify-between text-dark-1" href="#">
                                         Du lịch Bắc Giang, tuyệt đối không được bỏ qua 5 điểm đến 'cực hot'
                                     </a>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="row y-gap-30 mt-30 pt-30 border-top-light">
+                    <div class="row y-gap-30 mt-30 pt-30 ">
 
                         <div class="col-lg-4 col-sm-6">
 
@@ -131,35 +122,31 @@
                 </div>
                 <div class="col-xl-3">
                     <div data="slide-up delay-3" class="sidebar -blog">
-
-                        <div class="sidebar__item pt-30">
-                            <h5 class="text-18 fw-500 mb-10">Danh mục</h5>
+                        <div class="">
+                            <h5 class="text-18 fw-700 mb-10">Danh mục</h5>
 
                             <div class="y-gap-5">
 
-                                <a class="d-flex items-center justify-between text-dark-1" href="#">
+                                <a href="./tourList.php" class="d-flex items-center justify-between text-dark-1" href="#">
                                     <span class="text-15 text-dark-1">Tour trong nước</span>
-                                    <span class="text-14 text-light-1">+</span>
                                 </a>
 
-                                <a class="d-flex items-center justify-between text-dark-1" href="#">
+                                <a href="./tourList.php" class="d-flex items-center justify-between text-dark-1" href="#">
                                     <span class="text-15 text-dark-1">Tour quốc tế</span>
-                                    <span class="text-14 text-light-1">+</span>
                                 </a>
 
-                                <a class="d-flex items-center justify-between text-dark-1" href="#">
+                                <a href="./mainLabor.php" class="d-flex items-center justify-between text-dark-1" href="#">
                                     <span class="text-15 text-dark-1">Xuất khẩu lao động</span>
-                                    <span class="text-14 text-light-1">+</span>
                                 </a>
 
-                                <a class="d-flex items-center justify-between text-dark-1" href="#">
+                                <a href="./insurance.php" class="d-flex items-center justify-between text-dark-1" href="#">
                                     <span class="text-15 text-dark-1">Bảo hiểm</span>
                                 </a>
                             </div>
                         </div>
 
                         <div class="sidebar__item">
-                            <h5 class="text-18 fw-500 mb-10">Bài viết được yêu thích</h5>
+                            <h5 class="text-18 fw-700 mb-10">Bài viết được yêu thích</h5>
 
                             <div class="row y-gap-20 pt-10">
 
@@ -167,7 +154,7 @@
                                     <div class="d-flex items-center">
                                         <img class="size-65 rounded-8" src="img/blog/sidebar/1.png" alt="image">
                                         <div class="ml-15">
-                                            <h5 class="text-15 lh-15 fw-500">5 làng nghề truyền thống nổi tiếng ở miền Bắc</h5>
+                                            <a class="text-15 lh-15 fw-500">5 làng nghề truyền thống nổi tiếng ở miền Bắc</a>
                                         </div>
                                     </div>
                                 </div>
@@ -176,7 +163,7 @@
                                     <div class="d-flex items-center">
                                         <img class="size-65 rounded-8" src="img/blog/sidebar/2.png" alt="image">
                                         <div class="ml-15">
-                                            <h5 class="text-15 lh-15 fw-500">5 làng nghề truyền thống nổi tiếng ở miền Bắc</h5>
+                                            <a class="text-15 lh-15 fw-500">5 làng nghề truyền thống nổi tiếng ở miền Bắc</a>
                                         </div>
                                     </div>
                                 </div>
@@ -185,7 +172,7 @@
                                     <div class="d-flex items-center">
                                         <img class="size-65 rounded-8" src="img/blog/sidebar/3.png" alt="image">
                                         <div class="ml-15">
-                                            <h5 class="text-15 lh-15 fw-500">5 làng nghề truyền thống nổi tiếng ở miền Bắc</h5>
+                                            <a class="text-15 lh-15 fw-500">5 làng nghề truyền thống nổi tiếng ở miền Bắc</a>
                                         </div>
                                     </div>
                                 </div>
