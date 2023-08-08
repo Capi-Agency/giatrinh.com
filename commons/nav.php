0d10526a1782f5284fe1d8f1eaaf72e20ae0e588
Nhân Vượng <52657226+vuonggluon@users.noreply.github.com>
2023-08-08 09:19:48 +0700

    Update Nav
    Layout theme gốc

---
+++ commons/nav.php
@@ -0,0 +1,65 @@
+<header data-add-bg="bg-dark-1" class="header bg-green js-header" data-x="header" data-x-toggle="is-menu-opened">
+      <div data-anim="fade" class="header__container px-30 sm:px-20 container">
+        <div class="row justify-between items-center">
+
+          <div class="col-auto">
+            <div class="d-flex items-center">
+              <a href="index.html" class="header-logo mr-20" data-x="header-logo" data-x-toggle="is-logo-dark">
+                <img src="img/general/logo-light.svg" alt="logo icon">
+                <img src="img/general/logo-dark.svg" alt="logo icon">
+              </a>
+              <div class="header-menu " data-x="mobile-menu" data-x-toggle="is-menu-active">
+                <div class="mobile-overlay"></div>
+
+                <div class="header-menu__content">
+                  <div class="mobile-bg js-mobile-bg"></div>
+
+                  <div class="menu js-navList">
+                    <ul class="menu__nav text-white -is-active">
+                      <li>
+                        <a href="index.html">Trang Chủ</a>
+                      </li>
+                      <li>
+                        <a href="hotel-list-1.html">
+                          Khách sạn
+                        </a>
+                      </li>
+                      <li>
+                        <a href="tour-list-1.html">Tour du lịch</a>
+                      </li>
+                      <li>
+                        <a href="blog-list-1.html">Blog</a>
+                      </li>
+                      <li>
+                        <a href="about.html">Về chúng tôi</a>
+                      </li>
+                      <li>
+                        <a href="contact.html">Liên hệ</a>
+                      </li>
+                    </ul>
+                  </div>
+
+                  <div class="mobile-footer px-20 py-20 border-top-light js-mobile-footer">
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="col-auto">
+            <div class="d-flex items-center">
+              <div class="d-flex items-center ml-20 is-menu-opened-hide md:d-none">
+                <a href="contact.html" class="button px-30 fw-400 text-14 -white bg-white h-50 text-dark-1">Liên Hệ Chúng Tôi</a>
+              </div>
+              <div class="d-none xl:d-flex x-gap-20 items-center pl-30 text-white" data-x="header-mobile-icons"
+                data-x-toggle="text-white">
+                <div><button class="d-flex items-center icon-menu text-inherit text-20"
+                    data-x-click="html, header, header-logo, header-mobile-icons, mobile-menu"></button></div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </header> 
