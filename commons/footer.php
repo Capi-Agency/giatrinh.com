09c21bbac4646096d9a85789740dca4be2708def
Nhân Vượng <52657226+vuonggluon@users.noreply.github.com>
2023-08-08 09:55:09 +0700

    Update files Base

---
+++ commons/footer.php
@@ -0,0 +1,126 @@
+<footer class="footer -type-1">
+      <div class="container">
+        <div class="pt-60 pb-60">
+          <div class="row y-gap-40 justify-between xl:justify-start">
+            <div class="col-xl-2 col-lg-4 col-sm-6">
+              <h5 class="text-16 fw-500 mb-30">Contact Us</h5>
+
+              <div class="mt-30">
+                <div class="text-14 mt-30">Toll Free Customer Care</div>
+                <a href="#" class="text-18 fw-500 text-blue-1 mt-5">+(1) 123 456 7890</a>
+              </div>
+
+              <div class="mt-35">
+                <div class="text-14 mt-30">Need live support?</div>
+                <a href="#" class="text-18 fw-500 text-blue-1 mt-5">hi@gotrip.com</a>
+              </div>
+            </div>
+
+            <div class="col-xl-2 col-lg-4 col-sm-6">
+              <h5 class="text-16 fw-500 mb-30">Company</h5>
+              <div class="d-flex y-gap-10 flex-column">
+                <a href="#">About Us</a>
+                <a href="#">Careers</a>
+                <a href="#">Blog</a>
+                <a href="#">Press</a>
+                <a href="#">Gift Cards</a>
+                <a href="#">Magazine</a>
+              </div>
+            </div>
+
+            <div class="col-xl-2 col-lg-4 col-sm-6">
+              <h5 class="text-16 fw-500 mb-30">Support</h5>
+              <div class="d-flex y-gap-10 flex-column">
+                <a href="#">Contact</a>
+                <a href="#">Legal Notice</a>
+                <a href="#">Privacy Policy</a>
+                <a href="#">Terms and Conditions</a>
+                <a href="#">Sitemap</a>
+              </div>
+            </div>
+
+            <div class="col-xl-2 col-lg-4 col-sm-6">
+              <h5 class="text-16 fw-500 mb-30">Other Services</h5>
+              <div class="d-flex y-gap-10 flex-column">
+                <a href="#">Car hire</a>
+                <a href="#">Activity Finder</a>
+                <a href="#">Tour List</a>
+                <a href="#">Flight finder</a>
+                <a href="#">Cruise Ticket</a>
+                <a href="#">Holiday Rental</a>
+                <a href="#">Travel Agents</a>
+              </div>
+            </div>
+
+            <div class="col-xl-2 col-lg-4 col-sm-6">
+              <h5 class="text-16 fw-500 mb-30">Mobile</h5>
+
+              <div class="d-flex items-center px-20 py-10 rounded-4 border-light">
+                <div class="icon-apple text-24"></div>
+                <div class="ml-20">
+                  <div class="text-14 text-light-1">Download on the</div>
+                  <div class="text-15 lh-1 fw-500">Apple Store</div>
+                </div>
+              </div>
+
+              <div class="d-flex items-center px-20 py-10 rounded-4 border-light mt-20">
+                <div class="icon-play-market text-24"></div>
+                <div class="ml-20">
+                  <div class="text-14 text-light-1">Get in on</div>
+                  <div class="text-15 lh-1 fw-500">Google Play</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="py-20 border-top-light">
+          <div class="row justify-between items-center y-gap-10">
+            <div class="col-auto">
+              <div class="row x-gap-30 y-gap-10">
+                <div class="col-auto">
+                  <div class="d-flex items-center">
+                    © 2022 GoTrip LLC All rights reserved.
+                  </div>
+                </div>
+
+                <div class="col-auto">
+                  <div class="d-flex x-gap-15">
+                    <a href="#">Privacy</a>
+                    <a href="#">Terms</a>
+                    <a href="#">Site Map</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-auto">
+              <div class="row y-gap-10 items-center">
+                <div class="col-auto">
+                  <div class="d-flex items-center">
+                    <button class="d-flex items-center text-14 fw-500 text-dark-1 mr-10">
+                      <i class="icon-globe text-16 mr-10"></i>
+                      <span class="underline">English (US)</span>
+                    </button>
+
+                    <button class="d-flex items-center text-14 fw-500 text-dark-1">
+                      <i class="icon-usd text-16 mr-10"></i>
+                      <span class="underline">USD</span>
+                    </button>
+                  </div>
+                </div>
+
+                <div class="col-auto">
+                  <div class="d-flex x-gap-20 items-center">
+                    <a href="#"><i class="icon-facebook text-14"></i></a>
+                    <a href="#"><i class="icon-twitter text-14"></i></a>
+                    <a href="#"><i class="icon-instagram text-14"></i></a>
+                    <a href="#"><i class="icon-linkedin text-14"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
---
+++ laborJapan.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'commons/head.php';?>
+<!-- Head -->
+<body>
+    <!-- Nav -->
+    <?php include 'commons/nav.php';?>
+
+    <!-- Body -->
+
+
+
+    <!-- Footer -->
+    <?php include 'commons/footer.php';?>
+</body>
+</html>
