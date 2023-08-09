254498a4e17394f3b24f410632955a11d5aad468
tunggg <tranhuutungkeu@gmail.com>
2023-08-10 00:08:21 +0700

    update laborTaiwan l1

--- laborTaiwan.php
+++ laborTaiwan.php
@@ -5,13 +5,197 @@
 <body>
     <!-- Nav -->
     <?php include 'commons/nav.php';?>
-
     <!-- Body -->
+    <section class="pt-90">
+      <div class="container">
+        <div class="row y-gap-30">
+          <div class="col-xl-8">
+            <div class="row y-gap-40">
+              <div class="article-inner ">
+		<header class="entry-header">
+	<div class="entry-header-text entry-header-text-top  text-center">
+	   	<h6 class="entry-category is-xsmall">
+	<a href="https://hotrotuvanxkld.com/category/cac-chuong-trinh-xuat-khau-lao-dong/" rel="category tag">Các Chương Trình Xuất Khẩu Lao Động</a>, <a href="https://hotrotuvanxkld.com/category/thong-bao-moi/" rel="category tag">Thông Báo Mới</a></h6>
+<h1 class="entry-title">Thông Báo Tuyển Dụng Xuất Khẩu Lao Động Đài Loan</h1>
+<!-- .entry-meta -->
+	</div><!-- .entry-header -->
+        </header><!-- post-header -->
+
+        <div class="entry-content single-page">
+
+<p><span style="font-size: 120%;">&nbsp; &nbsp; Trường Cao đẳng Kinh tế Công nghệ Hà Nội Trực thuộc Bộ Lao động Thương binh và Xã hội Thông báo tuyển sinh Chương Trình đi&nbsp;Xuất Khẩu Lao Động Đài Loan. Chi Phí đi&nbsp;Xuất Khẩu Lao Động Đài Loan chuẩn theo quy định từ Bộ LĐTBXH (Cam kết không phát sinh). Cam kết hỗ trợ việc làm với thu nhập ổn định. Hỗ trợ làm đầy đủ giấy tờ thủ tục.</span></p>
+<p style="text-align: center;"><span style="font-size: 160%; color: #ed1c24;"><strong>THÔNG BÁO TUYỂN DỤNG XUẤT KHẨU LAO ĐỘNG ĐI ĐÀI LOAN</strong></span></p>
+<p style="text-align: center;"><span style="font-size: 130%; color: #ed1c24;"><strong>Theo Chương trình Hỗ trợ đi Xuất Khẩu Lao Động&nbsp;Từ Bộ Lao Động Thương Binh Xã Hội</strong></span></p>
+<p><span style="font-size: 120%;">&nbsp; &nbsp; Trường Cao đẳng Kinh tế Công nghệ Hà Nội được thành lập theo Quyết định số 594/QĐ-LĐTBXH ngày 12 tháng 5 năm 2010 của Bộ Lao động Thương binh và Xã hội. Thực hiện chủ trương của Nhà nước về việc nâng cao nguồn nhân lực trong nước, Trường Cao đẳng Kinh tế Công nghệ Hà Nội thông báo tuyển&nbsp;dụng&nbsp;Xuất Khẩu Lao Động đi Đài Loan. Để biết chi tiết về các đơn hàng của chương trình hãy gọi đến&nbsp;<span style="color: #ed1c24;"><strong>Hotline: 0862.416.198</strong></span>&nbsp;Dưới đây là một số thông tin cơ bản.</span></p>
+<p>&nbsp;</p>
+<p><span style="font-size: 120%; color: #ed1c24;"><strong>CHƯƠNG TRÌNH XUẤT KHẨU LAO ĐỘNG ĐI ĐÀI LOAN</strong></span></p>
+<p><span style="font-size: 120%; color: #008000;"><strong>Đối tượng và ngành nghề tuyển dụng cơ bản:</strong></span></p>
+<p><span style="font-size: 120%;">– Nam, nữ: Tốt nghiệp PTTH hoặc các Trường Trung cấp, Cao đẳng, Đại học.</span></p>
+<p><span style="font-size: 120%;">– Độ tuổi: từ 18&nbsp;– 40</span></p>
+<p><span style="font-size: 120%;">– Sức khỏe tốt, không mắc các bệnh truyền nhiễm: Viêm gan B, C, HIV…</span></p>
+<p><span style="font-size: 120%;">– Ý thức làm việc chăm chỉ, tính kỷ luật cao, có kinh nghiệm làm việc, có bảo hiểm xã hội 2 năm trở lên là một lợi thế.</span></p>
+<p><span style="font-size: 120%; color: #008000;"><strong>Ngành nghề và số lượng:</strong></span></p>
+<p><span style="font-size: 120%;">– Ngành nghề:&nbsp;Xây Dựng, Công xưởng.</span></p>
+<p><span style="font-size: 120%;">– Số lượng: Lớn hơn 10.</span></p>
+<p><span style="font-size: 120%; color: #008000;"><strong>Quyền lợi&nbsp;và Các điều kiện khác</strong></span></p>
+<p><span style="font-size: 120%;">– Được đi Xuất Khẩu Lao Động theo Chương trình và Chi Phí quy định từ Bộ Lao Động Thương Binh Xã Hội.</span></p>
+<p><span style="font-size: 120%;">– Hỗ trợ đầy đủ giấy tờ thủ tục khi đi Xuất Khẩu Lao Động. Hỗ trợ vay ngân hàng khi&nbsp;Người lao động&nbsp;yêu cầu.</span></p>
+<p><span style="font-size: 120%;">– Nhà trường cùng Bộ LĐTBXH Cam kết hỗ trợ việc làm với thu nhập ổn định cho&nbsp;người lao động đi Xuất khẩu.&nbsp;Vừa có tiền ăn, ở, chi tiêu và có thêm tiền tích lũy.</span></p>
+<p><span style="font-size: 120%;">–&nbsp;Lương làm thêm nhiều, các chế độ đãi ngộ tốt và các quyền lợi theo đúng pháp luật của nước bạn đi Xuất Khẩu Lao Động.</span></p>
+<p><span style="font-size: 120%;">– Điều kiện xuất cảnh: Cần có chứng chỉ tiếng của nước bạn đi&nbsp;Xuất Khẩu Lao Động.</span></p>
+<p><span style="font-size: 120%;">– Thời gian xuất cảnh: 3 – 4 tháng sau khi trúng tuyển.</span></p>
+<p><span style="font-size: 120%;">– Hợp đồng: Tối thiểu 3 năm, sau đó vẫn được tiếp tục gia hạn hợp đồng.</span></p>
+<p><span style="font-size: 120%; color: #0000ff;"><em><strong>– Liên tục nhận hồ hơ và thi tuyển nhiều đợt trong năm.</strong></em></span></p>
+<p>&nbsp;</p>
+<p><span style="font-size: 120%; color: #008000;"><strong><span style="color: #ed1c24;">QUY TRÌNH ĐI XUẤT KHẨU LAO ĐỘNG CƠ BẢN</span>&nbsp;<span style="color: #282828;"><em>(Đây là&nbsp;quy trình&nbsp;cơ bản, các nước khác nhau thời gian học có thể khác nhau. Hãy gọi vào Hotline để được tư vẫn nhanh và hỗ&nbsp;trợ miễn phí 24/7)</em></span></strong></span></p>
+<p><span style="font-size: 120%;">– Học viên quan tâm đến Xuất Khẩu Lao Động gọi điện đến Hotline sẽ được tư vấn hướng dẫn chuẩn bị hồ sơ giấy tờ cần thiết theo mẫu của nhà trường.</span></p>
+<p><span style="font-size: 120%;">– Tiếp theo bạn (Có thể đi cùng phụ huynh) mang hồ sơ đến trường để được tư vấn trực tiếp. Dựa vào trình độ học vấn, bằng cấp, kinh nghiệm làm việc, khả năng và tình hình tài chính của bạn, cán bộ tư vấn sẽ giúp bạn lựa chọn quyết định phù hợp nhất.</span></p>
+<p><span style="font-size: 120%;">– Khi đã chọn được&nbsp;chương chình và nước&nbsp;phù hợp để đi Xuất Khẩu Lao Động. Bạn cần nộp tiền đặt cọc và Nhà trường sẽ chuẩn bị cho bạn toàn bộ giấy tờ thủ tục liên quan.</span></p>
+<p><span style="font-size: 120%;">– Trong thời gian chờ bay bạn cần phải hoàn trình khóa học Tiếng và tác phong từ 4 – 9 tháng tại trường (Xuất Khẩu Lao Động Nghề cần thêm chứng chỉ nghề). Trong quá trình học bạn sẽ được hướng dẫn toàn bộ về những vấn đề cần chú ý và kinh nghiệm khi đi&nbsp;Xuất Khẩu Lao Động&nbsp;ở nước Ngoài.</span></p>
+<p><span style="font-size: 120%;">– Sau khi hoàn thành khóa học và các thủ tục liên quan bạn sẽ được Nhà trường chuẩn bị toàn bộ thủ tục bao gồm: đặt vé máy bay, đăng ký chuyến bay phù hợp, đưa đón&nbsp;người lao động&nbsp;tại sân bay, sắp xếp chỗ ăn ở cho&nbsp;người lao động&nbsp;theo yêu cầu… Và tất cả các vấn đề liên quan khác.</span></p>
+<p><span style="font-size: 120%;">– Khi đã qua đến nước bạn Xuất Khẩu Lao Động. Bạn sẽ theo chương trình&nbsp;làm việc&nbsp;tại nơi bạn quy định. Bạn sẽ được cung cấp số điện thoại Hotline, địa chỉ văn phòng Hỗ trợ tại nước sở tại. Bạn có thể đăng ký việc làm thêm tại đây và có thể ứng tiền tại Văn phòng khi chưa có tiền tiêu dùng.</span></p>
+<p><span style="font-size: 120%;">Trên đây là&nbsp;quy trình&nbsp;cơ bản, các nước khác nhau thời gian có thể khác nhau. Để biết thông tin chi tiết về nước bạn muốn đi Xuất Khẩu Lao Động. Hãy gọi vào Hotline để được tư vẫn nhanh và hỗ&nbsp;trợ miễn phí 24/7.</span></p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p><span style="font-size: 120%; color: #ed1c24;"><strong>THÔNG TIN LIÊN HỆ</strong></span></p>
+<p><span style="font-size: 120%; color: #008000;"><strong>Phòng Tuyển&nbsp;Dụng Xuất Khẩu Lao Động&nbsp;Trường Cao Đẳng Kinh Kế Công Nghệ Hà Nội</strong></span><br>
+<span style="font-size: 120%;"><span style="color: #0000ff;"><strong>Địa Chỉ:</strong></span>&nbsp;Văn Tiến Dũng, Minh Khai, Bắc Từ Liêm, Hà Nội.</span><br>
+<span style="font-size: 120%;"><strong><span style="color: #0000ff;">Hotline:</span> <span style="color: #ed1c24;">0862.416.198</span></strong></span><br>
+<span style="font-size: 120%;"><strong><span style="color: #0000ff;">Website:</span></strong>&nbsp;<a href="https://www.hotrotuvanxkld.com">www.hotrotuvanxkld.com</a></span><br>
+<span style="font-size: 120%;"><strong><span style="color: #0000ff;">Fanpage:</span></strong>&nbsp;Facebook.com/HoTroDangKyHoc</span></p>
+<p><span style="font-size: 120%;">Khi quan tâm Du học hay Xuất Khẩu Lao Động hãy gọi ngay tới&nbsp;<strong><span style="color: #ed1c24;">Hotline 24/7: 0862.416.198</span></strong>&nbsp;bạn sẽ được tư vấn nhanh và hỗ trợ một cách tốt nhất.</span></p>
+<p><span style="font-size: 120%;">Hoặc truy cập: Facebook.com/HoTroDangKyHoc&nbsp;và để lại lời nhắn bạn sẽ nhận được câu trả lời trong thời gian sớm nhất.</span></p>
 
 
+<div class="blog-share text-center"><div class="is-divider medium"></div>
+	<div class="social-icons share-icons share-row relative icon-style-outline ">
+		  		  		  <a href="//www.facebook.com/sharer.php?u=https://hotrotuvanxkld.com/thong-bao-tuyen-dung-xuat-khau-lao-dong-dai-loan/" data-label="Facebook" onclick="window.open(this.href,this.title,'width=500,height=500,top=300px,left=300px');  return false;" rel="nofollow" target="_blank" class="icon button circle is-outline tooltip facebook tooltipstered"><i class="icon-facebook"></i></a>
+		            <a href="//twitter.com/share?url=https://hotrotuvanxkld.com/thong-bao-tuyen-dung-xuat-khau-lao-dong-dai-loan/" onclick="window.open(this.href,this.title,'width=500,height=500,top=300px,left=300px');  return false;" rel="nofollow" target="_blank" class="icon button circle is-outline tooltip twitter tooltipstered"><i class="icon-twitter"></i></a>
+                    <a href="//pinterest.com/pin/create/button/?url=https://hotrotuvanxkld.com/thong-bao-tuyen-dung-xuat-khau-lao-dong-dai-loan/&amp;media=https://hotrotuvanxkld.com/wp-content/uploads/2019/08/10613137_735570313201605_5551887717403862697_n.jpg&amp;description=Th%C3%B4ng%20B%C3%A1o%20Tuy%E1%BB%83n%20D%E1%BB%A5ng%20Xu%E1%BA%A5t%20Kh%E1%BA%A9u%20Lao%20%C4%90%E1%BB%99ng%20%C4%90%C3%A0i%20Loan" onclick="window.open(this.href,this.title,'width=500,height=500,top=300px,left=300px');  return false;" rel="nofollow" target="_blank" class="icon button circle is-outline tooltip pinterest tooltipstered"><i class="icon-pinterest"></i></a>
+              </div>
+    
+    </div></div>
+
+<footer class="entry-meta text-center">
+Mục nhập này đã được đăng trong <a href="https://hotrotuvanxkld.com/category/cac-chuong-trinh-xuat-khau-lao-dong/" rel="category tag">Các Chương Trình Xuất Khẩu Lao Động</a>, <a href="https://hotrotuvanxkld.com/category/thong-bao-moi/" rel="category tag">Thông Báo Mới</a>. Đánh dấu <a href="https://hotrotuvanxkld.com/thong-bao-tuyen-dung-xuat-khau-lao-dong-han-quoc/" title="Đường dẫn tĩnh tới Thông Báo Tuyển Dụng Xuất Khẩu Lao Động Hàn Quốc" rel="bookmark">đường dẫn tĩnh</a>.</footer><!-- .entry-meta -->
+
+
+
+        <nav role="navigation" id="nav-below" class="navigation-post">
+	<div class="flex-row next-prev-nav bt bb">
+		<div class="flex-col flex-grow nav-prev text-left">
+			    <div class="nav-previous"><a href="https://hotrotuvanxkld.com/thong-bao-tuyen-dung-xuat-khau-lao-dong-nhat-ban/" rel="prev"><span class="hide-for-small"><i class="icon-angle-left"></i></span> Thông Báo Tuyển Dụng Lao Động Đi Làm Việc Tại Nhật Bản</a></div>
+		</div>
+		<div class="flex-col flex-grow nav-next text-right">
+			    <div class="nav-next"><a href="https://hotrotuvanxkld.com/thong-bao-tuyen-dung-xuat-khau-lao-dong-dai-loan/" rel="next">Thông Báo Tuyển Dụng Xuất Khẩu Lao Động Đài Loan <span class="hide-for-small"><i class="icon-angle-right"></i></span></a></div>		</div>
+	</div>
+
+	    </nav><!-- #nav-below -->
+
+    	</div>
+            </div>
+          </div>
+
+          <div class="col-xl-4">
+            <div class="ml-50 lg:ml-0">
+              <div class="px-30 py-30 border-light rounded-4 shadow-4">
+                <div class="d-flex items-center justify-between">
+                  <div class="">
+                    <span class="text-20 fw-500">ĐĂNG KÝ TƯ VẤN NHANH</span>
+                    <span class="text-14 text-light-1 ml-5"></span>
+                  </div>
+
+              
+                </div>
+
+                <div class="row y-gap-20 pt-30">
+            
+                  <div class="col-12">
+                  <div class="row y-gap-20 pt-20">
+            <div class="col-12">
+
+              <div class="form-input ">
+                <input type="text" required>
+                <label class="lh-1 text-16 text-light-1">Tên đầy đủ</label>
+              </div>
+
+            </div>
+            <div class="col-12">
+
+              <div class="form-input ">
+                <input type="text" required>
+                <label class="lh-1 text-16 text-light-1">Email</label>
+              </div>
+
+            </div>
+            <div class="col-12">
+
+              <div class="form-input ">
+                <input type="text" required>
+                <label class="lh-1 text-16 text-light-1">Tiêu đề</label>
+              </div>
+
+            </div>
+            <div class="col-12">
+
+              <div class="form-input ">
+                <textarea required rows="4"></textarea>
+                <label class="lh-1 text-16 text-light-1">Nội dung</label>
+              </div>
+
+            </div>
+            <div class="col-auto">
+
+              <a href="#" class="button px-24 h-50 w-100 -dark-1 bg-blue-1 text-white">
+                Gửi tin nhắn <div class="icon-arrow-top-right ml-15"></div>
+              </a>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    </section>
+    <footer class="entry-meta text-center">
+Mục nhập này đã được đăng trong <a href="https://hotrotuvanxkld.com/category/cac-chuong-trinh-xuat-khau-lao-dong/" rel="category tag">Các Chương Trình Xuất Khẩu Lao Động</a>, <a href="https://hotrotuvanxkld.com/category/thong-bao-moi/" rel="category tag">Thông Báo Mới</a>. Đánh dấu <a href="https://hotrotuvanxkld.com/thong-bao-tuyen-dung-xuat-khau-lao-dong-han-quoc/" title="Đường dẫn tĩnh tới Thông Báo Tuyển Dụng Xuất Khẩu Lao Động Hàn Quốc" rel="bookmark">đường dẫn tĩnh</a>.</footer>
+<section class="layout-pt-md layout-pb-lg">
+      <div data-anim-wrap="" class="container animated">
+
+
+        <div class="row y-gap-30 pt-40">
+
+          <div data-anim-child="slide-up delay-2" class="col-lg-6 is-in-view">
+            <a href="https://creativelayers.net/themes/gotrip-html/blog-single-1.html" class="row y-gap-20 items-center">
+              
+
+              <div class="col">
+                
+                <h4 class="text-22 fw-600 text-dark-1 mt-10"> Thông Báo Tuyển Dụng Lao Động Đi Làm Việc Tại Nhật Bản</h4>
+             
+              </div>
+            </a>
+          </div>
+
+          <div data-anim-child="slide-up delay-3" class="col-lg-6 is-in-view">
+            <a href="https://creativelayers.net/themes/gotrip-html/blog-single-1.html" class="row y-gap-20 items-center">
+             
+
+              <div class="col">
+               
+                <h4 class="text-22 fw-600 text-dark-1 mt-10">Thông Báo Tuyển Dụng Xuất Khẩu Lao Động Đài Loan </h4>
+             
+              </div>
+            </a>
+          </div>
+
+        </div>
+      </div>
+    </section>   
     <!-- Email -->
     <?php include 'commons/email.php';?>
     <!-- Footer -->
     <?php include 'commons/footer.php';?>
+
 </body>
 </html>
