72bb442a2d9cfc752ef04a9c2f0ab0a3fded6add
tunggg <tranhuutungkeu@gmail.com>
2023-08-10 09:29:18 +0700

    update list

--- aboutUs.php
+++ aboutUs.php
@@ -7,7 +7,232 @@
     <?php include 'commons/nav.php';?>
 
     <!-- Body -->
+    <section class="layout-pb-md">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="relative d-flex">
+              <img src="img/pages/destinations/1.png" alt="image" class="col-12 rounded-4">
 
+              <div class="absolute z-2 px-50 py-60">
+                <h1 class="text-50 fw-600 text-white">Explore London</h1>
+                <div class="text-white">Explore deals, travel guides and things to do in London</div>
+              </div>
+
+              <div class="absolute d-flex justify-end items-end col-12 h-full z-1 px-10 py-10">
+                <button class="button -md -blue-1 bg-white text-dark-1 text-14 fw-500">See All 90 Photos</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row x-gap-20 y-gap-20 items-center pt-20">
+
+          <div class="col">
+            <button class="d-flex flex-column justify-center px-20 py-15 rounded-4 border-light text-16 lh-14 fw-500 col-12">
+              <i class="icon-bed text-25 mb-10"></i>
+              Hotel
+            </button>
+          </div>
+
+          <div class="col">
+            <button class="d-flex flex-column justify-center px-20 py-15 rounded-4 border-light text-16 lh-14 fw-500 col-12">
+              <i class="icon-destination text-25 mb-10"></i>
+              Tour
+            </button>
+          </div>
+
+          <div class="col">
+            <button class="d-flex flex-column justify-center px-20 py-15 rounded-4 border-light text-16 lh-14 fw-500 col-12">
+              <i class="icon-ski text-25 mb-10"></i>
+              Activity
+            </button>
+          </div>
+
+          <div class="col">
+            <button class="d-flex flex-column justify-center px-20 py-15 rounded-4 border-light text-16 lh-14 fw-500 col-12">
+              <i class="icon-home text-25 mb-10"></i>
+              Holiday Rentals
+            </button>
+          </div>
+
+          <div class="col">
+            <button class="d-flex flex-column justify-center px-20 py-15 rounded-4 border-light text-16 lh-14 fw-500 col-12">
+              <i class="icon-car text-25 mb-10"></i>
+              Car
+            </button>
+          </div>
+
+          <div class="col">
+            <button class="d-flex flex-column justify-center px-20 py-15 rounded-4 border-light text-16 lh-14 fw-500 col-12">
+              <i class="icon-yatch text-25 mb-10"></i>
+              Cruise
+            </button>
+          </div>
+
+          <div class="col">
+            <button class="d-flex flex-column justify-center px-20 py-15 rounded-4 border-light text-16 lh-14 fw-500 col-12">
+              <i class="icon-tickets text-25 mb-10"></i>
+              Flights
+            </button>
+          </div>
+
+        </div>
+
+        <div class="row y-gap-20 pt-40">
+          <div class="col-auto">
+            <h2 class="">What to know before visiting London</h2>
+          </div>
+
+          <div class="col-xl-8">
+            <p class="text-15 text-dark-1">
+              London is a shining example of a metropolis at the highest peak of modernity and boasts an economy and cultural diversity that’s the envy of other global superpowers.
+              <br><br>
+              Take the opportunity to acquaint yourself with its fascinating history chronicled by institutions like the British Museum as well as see how far it has come by simply riding the Tube and passing by celebrated landmarks like Buckingham Palace, Westminster Abbey, and marvels like Big Ben, the London Eye, and the Tower Bridge.
+              <br><br>
+              You can also immerse yourself in its ever-evolving and impactful culture by visiting places like the National Gallery, the Tate Modern, West End, Abbey Road, the Royal Albert Hall, Oxford Street and the Westfield Shopping Centers, and areas referenced and seen in literature and film.
+            </p>
+
+            <a href="#" class="d-block text-14 fw-500 text-blue-1 underline mt-20">Show More</a>
+          </div>
+
+          <div class="col-xl-4">
+            <div class="relative d-flex ml-35">
+              <img src="img/pages/destinations/map.png" alt="image" class="col-12 rounded-4">
+
+              <div class="absolute d-flex justify-center items-end col-12 h-full z-1 px-35 py-20">
+                <button class="button h-50 px-25 -blue-1 bg-white text-dark-1 text-14 fw-500 col-12">
+                  <i class="icon-eye text-18 mr-10"></i>
+                  See popular activities on the map
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="pt-30 mt-30 border-top-light"></div>
+
+        <div class="row y-gap-20">
+          <div class="col-12">
+            <h2 class="text-22 fw-500">Local weather</h2>
+          </div>
+
+
+          <div class="col-xl-3 col-6">
+            <div class="text-15 lh-1 fw-500">
+              DEC - FEB
+            </div>
+            <div class="text-22 fw-500 mt-10">
+              7° <span class="text-light-1 ml-15">3°</span>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-6">
+            <div class="text-15 lh-1 fw-500">
+              MAR - MAY
+            </div>
+            <div class="text-22 fw-500 mt-10">
+              17° <span class="text-light-1 ml-15">3°</span>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-6">
+            <div class="text-15 lh-1 fw-500">
+              JUN - AUG
+            </div>
+            <div class="text-22 fw-500 mt-10">
+              27° <span class="text-light-1 ml-15">3°</span>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-6">
+            <div class="text-15 lh-1 fw-500">
+              SEP - NOV
+            </div>
+            <div class="text-22 fw-500 mt-10">
+              7° <span class="text-light-1 ml-15">3°</span>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="pt-30 mt-30 border-top-light"></div>
+
+        <div class="row y-gap-20">
+          <div class="col-12">
+            <h2 class="text-22 fw-500">General info</h2>
+          </div>
+
+          <div class="col-xl-3 col-6">
+            <div class="text-15">Time Zone</div>
+            <div class="fw-500">GMT +00:00</div>
+            <div class="text-15 text-light-1">3 hours behind</div>
+          </div>
+
+          <div class="col-xl-3 col-6">
+            <div class="text-15">Currency</div>
+            <div class="fw-500">British Pound</div>
+            <div class="text-15 text-light-1">1USD = 0.76GBP</div>
+          </div>
+
+          <div class="col-xl-3 col-6">
+            <div class="text-15">Best time to visit</div>
+            <div class="row y-gap-20">
+              <div class="col-auto">
+                <div class="fw-500">JUN</div>
+                <div class="text-15 text-light-1">The Queen's Birthday</div>
+              </div>
+              <div class="col-auto">
+                <div class="fw-500">JUN</div>
+                <div class="text-15 text-light-1">The Queen's Birthday</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="mt-30 border-top-light"></div>
+      </div>
+    </section>
+    <div class="inner">
+			<div class="about-desc-block">
+				<h1>Giới thiệu</h1>
+				<div class="rte">
+					<p><strong>Công ty Lữ hành Hanoitourist</strong>&nbsp;là đơn vị trực thuộc Tổng Công ty Du lịch Hà Nội - doanh nghiệp nhà nước có vị thế hàng đầu trong các lĩnh vực: kinh doanh lữ hành quốc tế, khách sạn, nhà hàng, vận chuyển, xuất khẩu lao động…</p><p>Tiền thân là Công ty Du lịch Hà Nội được thành lập từ ngày 25/3/1963, đến ngày 12/ 7/2004, UBND Thành phố Hà Nội ra Quyết định thành lập Tổng công ty Du lịch Hà Nội, thí điểm hoạt động theo mô hình Công ty mẹ - công ty con, trên cơ sở tập hợp một số doanh nghiệp kinh doanh du lịch trên địa bàn thành phố với mục tiêu tập trung xây dựng một Tổng công ty du lịch lớn, có thương hiệu mạnh, hoạt động đa ngành nghề, đa sở hữu, có sức cạnh tranh cao và hội nhập kinh tế quốc tế hiệu quả, phù hợp với yêu cầu phát triển ngành du lịch và nền kinh tế Thủ đô.</p><p>Qua hơn 50 năm xây dựng và phát triển, từ một Công ty Du lịch, đến nay đã trở thành một Tổng Công ty lớn mạnh gồm 38 Công ty thành viên, Công ty liên doanh liên kết với nước ngoài và trong nước, Công ty cổ phần, Đơn vị phụ thuộc... với hơn 4000 CBCNV tham gia hoạt động sản xuất kinh doanh đa ngành nghề:</p><p>- Trực tiếp tổ chức các hoạt động kinh doanh: Kinh doanh lữ hành quốc tế và nội địa, Kinh doanh khách sạn và vận chuyển khách du lịch bằng đường bộ, đường sông và trên Vịnh, Kinh doanh các dịch vụ du lịch và dịch vụ đại lý bán vé máy bay; Đầu tư, liên doanh liên kết với các thành phần kinh tế trong và ngoài nước để phát triển hạ tầng du lịch: Khu du lịch, Khu vui chơi giải trí, Khách sạn, Nhà hàng, Trung tâm thương mại dịch vụ; Kinh doanh dịch vụ: ăn uống, hướng dẫn du lịch, Phiên dịch, thông tin, vui chơi giải trí, cho người nước ngoài thuê nhà, Cho thuê các phương tiện vận tải;</p><p>- Kinh doanh nhà hàng, đồ lưu niệm; Cho thuê văn phòng; Dịch vụ quảng cáo, Tổ chức hội chợ, triển lãm chuyên đề, hội nghị, hội thảo và Dịch vụ xúc tiến du lịch – thương mại;&nbsp;</p><p>- Kinh doanh xuất khẩu lao động và chuyên gia; Tư vấn du học nước ngoài;</p><p>&nbsp;Tổng Công ty Du lịch Hà Nội từ khi thành lập đã rất quan tâm đến việc tổ chức đào tạo, dạy nghề, bồi dưỡng các nghiệp vụ về: du lịch, khách sạn, hướng dẫn viên du lịch, ngoại ngữ, buồng, bàn, bar, lễ tân, giúp việc gia đình, phục vụ cho các nhiệm vụ sản xuất kinh doanh của Tổng Công ty hướng đến sự phát triển bền vững trong tương lai và đáp ứng nhu cầu ngày càng cao của xã hội; Nhằm đa dạng hóa sản phẩm, Tổng Công ty Du lịch Hà Nội còn thực hiện chức năng sản xuất kinh doanh và đầu tư trong các lĩnh vực: Thương mại, Tài chính, Công nghiệp chế biến nông lâm hải sản, xây dựng phát triển nhà và khu đô thị… phục vụ nhiệm vụ phát triển Du lịch và nhiệm vụ phát triển kinh tế xã hội của Thủ đô.</p><p>&nbsp; &nbsp; &nbsp; &nbsp; Ngoài ra, Tổng công ty còn tham gia với các cơ quan chức năng xây dựng quy hoạch và kế hoạch phát triển ngành Du lịch theo định hướng phát triển kinh tế xã hội của Thành phố cũng như của Chính phủ; Lập, quản lý, tổ chức triển khai thực hiện các dự án đầu tư xây dựng cơ sở hạ tầng phát triển du lịch bằng nguồn vốn ngân sách Nhà nước cấp, vốn vay, vốn huy động của Tổng Công ty.</p><p>Trong đó ngành nghề chính là Kinh doanh lữ hành quốc tế và nội địa, Kinh doanh khách sạn, Vận chuyển khách du lịch, Kinh doanh các dịch vụ du lịch, Đầu tư, Liên doanh liên kết với các tổ chức kinh tế trong và ngoài nước để phát triển cơ sở hạ tầng du lịch và dịch vụ du lịch.</p><p>Tổng Công ty Du lịch Hà Nội không ngừng cải tiến chất lượng dịch vụ, nâng cấp cơ sở hạ tầng, phát triển nhiều mô hình sản phẩm mới cũng như tăng cường tuyên truyền - quảng bá - tiếp thị đến khách hàng.</p><p>&nbsp; &nbsp; &nbsp; &nbsp; Nỗ lực hoạt động đã mang lại cho Tổng Công ty nhiều Huân chương và danh hiệu cao quý: Huân ch­ương Lao động hạng Ba (năm 1980; năm 2010 ), Huân chư­ơng Lao động hạng Hai (năm 1985); Huân ch­ương Lao động hạng Nhất (năm 2002), 2 Cờ Luân l­ưu của Chính phủ, 1 Cờ thi đua của Bộ Văn hoá, Thông tin, Thể thao và Du lịch (năm 1990), 4 Cờ thi đua xuất sắc của UBND Thành phố Hà Nội, 4 Cờ thi đua của Tổng cục Du lịch Việt Nam và nhiều bằng khen, danh hiệu khác… Đặc biệt, Thương hiệu mang tên Hanoitourist và địa chỉ trụ sở chính tại 18 Lý Thường Kiệt đã hình thành và có chỗ đứng vững chắc trong tâm trí khách hàng.</p>
+				</div>
+			</div>
+			<div class="map-block"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d553.6156867426013!2d105.85539232066483!3d21.02197720893829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135abecf5c5a0a9%3A0x663f15b4628b3028!2zQ8O0bmcgdHkgTOG7ryBow6BuaCBIYW5vaXRvdXJpc3Q!5e0!3m2!1sen!2s!4v1680236405258!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+			<div class="about-history">
+				<h2 class="about-history-title text-center">Lịch sử công ty</h2>
+				<div class="about-history-owl owl-carousel owl-loaded owl-drag">
+					
+					
+				<div class="owl-stage-outer"><div class="owl-stage" style="transform: translate3d(0px, 0px, 0px); transition: all 0s ease 0s; width: 367px;"><div class="owl-item active current current2" style="width: 183.333px;"><div class="about-item" data-index="0">
+						<span>1963</span>
+					</div></div><div class="owl-item active" style="width: 183.333px;"><div class="about-item" data-index="1">
+						<span>2004</span>
+					</div></div></div></div><div class="owl-nav disabled"><button type="button" role="presentation" class="owl-prev disabled"><svg class="svg-inline--fa fa-angle-left fa-w-8" aria-hidden="true" data-prefix="fas" data-icon="angle-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" data-fa-i2svg=""><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg><!-- <i class="fas fa-angle-left"></i> --></button><button type="button" role="presentation" class="owl-next disabled"><svg class="svg-inline--fa fa-angle-right fa-w-8" aria-hidden="true" data-prefix="fas" data-icon="angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" data-fa-i2svg=""><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg><!-- <i class="fas fa-angle-right"></i> --></button></div><div class="owl-dots disabled"></div></div>
+				<div class="about-history-sync owl-carousel owl-loaded">
+					
+					
+				<div class="owl-stage-outer"><div class="owl-stage" style="transform: translate3d(0px, 0px, 0px); transition: fadeIn 0s ease 0s; width: 1800px;"><div class="owl-item active" style="width: 900px;"><div class="about-sync" data-index="0">
+						<h3>Công ty Du lịch Hà Nội</h3>
+						<div class="time">
+							<time>- 1963</time>
+						</div>
+						<div class="desc">
+							Công ty Du lịch Hà Nội được thành lập từ ngày 25/3/1963
+						</div>
+					</div></div><div class="owl-item" style="width: 900px;"><div class="about-sync" data-index="1">
+						<h3>Tổng Công ty Du lịch Hà Nội</h3>
+						<div class="time">
+							<time>- 2004</time>
+						</div>
+						<div class="desc">
+							Tổng công ty Du lịch Hà Nội (Hanoitourist Corporation) được thành lập theo Quyết định số 99/2004/QĐ-TTg ngày 01/6/2004 của Thủ tướng Chính phủ và Quyết định số 106/2004/QĐ-UB ngày 12/7/2004 của UBND Thành phố Hà Nội. Tổng công ty hoạt động, và quản lý theo mô hình Công ty mẹ - Công ty con, trong đó Công ty du lịch Hà Nội (Hanoitourist) làm Công ty mẹ và một số công ty du lịch trên địa bàn Thủ đô là công ty thành viên và trực thuộc. Công ty Du lịch Hà Nội - Công ty mẹ - (tiền thân là Chi nhánh của Công ty Du lịch Việt Nam) được thành lập năm 1963.
+						</div>
+					</div></div></div></div><div class="owl-nav disabled"><button type="button" role="presentation" class="owl-prev"><span aria-label="Previous">‹</span></button><button type="button" role="presentation" class="owl-next"><span aria-label="Next">›</span></button></div><div class="owl-dots disabled"></div></div>
+			</div>
+		</div>
     <!-- Email -->
     <?php include 'commons/email.php';?>
 
