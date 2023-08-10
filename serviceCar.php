8d6a117729f2c930b2707d108ccbce023b088e21
tunggg <tranhuutungkeu@gmail.com>
2023-08-10 11:59:18 +0700

    update layout

--- serviceCar.php
+++ serviceCar.php
@@ -7,6 +7,270 @@
     <?php include 'commons/nav.php';?>
 
     <!-- Body -->
+    <div class="header-margin"></div>
+    <section class="py-10 d-flex items-center bg-light-2">
+        <div class="container">
+            <div class="row y-gap-10 items-center justify-between">
+                <div class="col-auto">
+                    <div class="row x-gap-10 y-gap-5 items-center text-14 text-light-1">
+                        <div class="col-auto">
+                            <div class="">Trang chủ</div>
+                        </div>
+                        <div class="col-auto">
+                            <div class="">&gt;</div>
+                        </div>
+                        <div class="col-auto">
+                            <div class="">Kinh nghiệm du lịch</div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+    
+    <section class="layout-pt-lg layout-pb-lg">
+      <div class="container">
+        <div class="row justify-center text-center">
+          <div class="col-auto">
+            <div class="sectionTitle -md">
+              <h2 class="sectionTitle__title">Frequently Asked Questions</h2>
+              <p class=" sectionTitle__text mt-5 sm:mt-0">Interdum et malesuada fames</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row y-gap-30 justify-center pt-40 sm:pt-20">
+          <div class="col-xl-8 col-lg-10">
+            <div class="accordion -simple row y-gap-20 js-accordion">
+
+              <div class="col-4">
+                <div class="accordion__item px-20 py-20 border-light rounded-4">
+                  <div class="accordion__button d-flex items-center">
+                    <div class="accordion__icon size-40 flex-center bg-light-2 rounded-full mr-20">
+                      <i class="icon-plus"></i>
+                      <i class="icon-minus"></i>
+                    </div>
+
+                    <div class="button text-dark-1">Hà Nội - Hạ Long</div>
+                  </div>
+
+                  <div class="accordion__content">
+                    <div class="pt-20 pl-60">
+                    <div id="elementor-tab-content-9101" class="eael-accordion-content clearfix active-default" data-tab="1" role="tabpanel" aria-labelledby="h-ni-h-long" style="display: block;"><p></p><p><img decoding="async" class="alignnone wp-image-17552 size-medium lazyloaded" src="https://dichung.vn/wp-content/uploads/2021/03/halong-473x315.jpg" alt="thue xe di ha long" width="473" height="315" sizes="(max-width: 473px) 100vw, 473px" srcset="https://dichung.vn/wp-content/uploads/2021/03/halong-473x315.jpg 473w, https://dichung.vn/wp-content/uploads/2021/03/halong.jpg 660w" data-ll-status="loaded"><noscript><img decoding="async" class="alignnone wp-image-17552 size-medium" src="https://dichung.vn/wp-content/uploads/2021/03/halong-473x315.jpg" alt="thue xe di ha long" width="473" height="315" srcset="https://dichung.vn/wp-content/uploads/2021/03/halong-473x315.jpg 473w, https://dichung.vn/wp-content/uploads/2021/03/halong.jpg 660w" sizes="(max-width: 473px) 100vw, 473px" /></noscript></p><div class="table-responsive wprt_style_display"><table style="border-collapse: collapse; width: 99.9459%;" class="table"><tbody><tr><td style="width: 37.3044%;">&nbsp;</td><td style="width: 29.3622%;"><strong>1 chiều</strong></td></tr><tr><td style="width: 37.3044%;">Xe 4 chỗ</td><td style="width: 29.3622%;">1.600.000đ</td></tr><tr><td style="width: 37.3044%;">Xe 7 chỗ</td><td style="width: 29.3622%;">1.800.000đ</td></tr><tr><td style="width: 37.3044%;">Xe 16 chỗ</td><td style="width: 29.3622%;">2.500.000đ</td></tr><tr><td style="width: 37.3044%;">Xe 29 chỗ</td><td style="width: 29.3622%;">Liên hệ</td></tr><tr><td style="width: 37.3044%;">Xe 35 chỗ</td><td style="width: 29.3622%;">Liên hệ</td></tr><tr><td style="width: 37.3044%;">Xe 45 chỗ</td><td style="width: 29.3622%;">Liên hệ</td></tr></tbody></table></div><p></p></div>      
+                </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-4">
+                <div class="accordion__item px-20 py-20 border-light rounded-4">
+                  <div class="accordion__button d-flex items-center">
+                    <div class="accordion__icon size-40 flex-center bg-light-2 rounded-full mr-20">
+                      <i class="icon-plus"></i>
+                      <i class="icon-minus"></i>
+                    </div>
+
+                    <div class="button text-dark-1">Hà Nội - Hạ Long</div>
+                  </div>
+
+                  <div class="accordion__content">
+                    <div class="pt-20 pl-60">
+                      <p class="text-15">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-4">
+                <div class="accordion__item px-20 py-20 border-light rounded-4">
+                  <div class="accordion__button d-flex items-center">
+                    <div class="accordion__icon size-40 flex-center bg-light-2 rounded-full mr-20">
+                      <i class="icon-plus"></i>
+                      <i class="icon-minus"></i>
+                    </div>
+
+                    <div class="button text-dark-1">Hà Nội - Hạ Long</div>
+                  </div>
+
+                  <div class="accordion__content">
+                    <div class="pt-20 pl-60">
+                      <p class="text-15">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="layout-pt-lg layout-pb-lg bg-blue-2">
+      <div class="container">
+        <div class="row justify-center">
+          <div class="col-xl-10 col-lg-11">
+            
+
+            <div class="bg-white rounded-4 mt-50">
+              <div class="layout-pt-lg layout-pb-lg px-50">
+               
+
+                <div class="row justify-between pt-50">
+                  <div class="col-auto">
+                    <div class="text-15 text-light-1">Invoice date:</div>
+                    <div class="text-15 fw-500 lh-15">03/10/2022</div>
+                  </div>
+
+                  <div class="col-xl-4">
+                    <div class="text-15 text-light-1">Due date:</div>
+                    <div class="text-15 fw-500 lh-15">03/10/2022</div>
+                  </div>
+                </div>
+
+                <div class="row justify-between pt-50">
+                  <div class="col-auto">
+                    <div class="text-20 fw-500">Supplier</div>
+                    <div class="text-15 fw-500 mt-20">Jobio LLC</div>
+                    <div class="text-15 text-light-1 mt-10">2301 Ravenswood Rd Madison,<br> WI 53711</div>
+                  </div>
+
+                  <div class="col-xl-4">
+                    <div class="text-20 fw-500">Customer</div>
+                    <div class="text-15 fw-500 mt-20">John Doe</div>
+                    <div class="text-15 text-light-1 mt-10">329 Queensberry Street, North Melbourne VIC 3051, Australia.</div>
+                  </div>
+                </div>
+
+                <div class="row pt-50">
+                  <div class="col-12">
+                    <table class="table col-12">
+                      <thead class="bg-blue-1-05 text-blue-1">
+                        <tr>
+                          <th>Description</th>
+                          <th>Price</th>
+                          <th>VAT (20%)</th>
+                          <th>Total</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+
+                        <tr>
+
+                          <td>Standard Plan</td>
+
+                          <td>$443.00</td>
+
+                          <td>$921.80</td>
+
+                          <td>$9243</td>
+
+                        </tr>
+
+                        <tr>
+
+                          <td>Extra Plan </td>
+
+                          <td>$413.00</td>
+
+                          <td>$912.80</td>
+
+                          <td>$5943</td>
+
+                        </tr>
+
+
+                        <tr>
+                          <td class="text-18 fw-500">Total Due</td>
+                          <td></td>
+                          <td></td>
+                          <td class="text-18 fw-500">$9,750</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+
+              <div class="border-top-light py-50">
+                <div class="row x-gap-60 y-gap-10 justify-center">
+                  <div class="col-auto">
+                    <a href="#" class="text-14">www.gotirp.com</a>
+                  </div>
+                  <div class="col-auto">
+                    <a href="#" class="text-14">invoice@gotrip.com</a>
+                  </div>
+                  <div class="col-auto">
+                    <a href="#" class="text-14">(123) 123-456</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="layout-pt-lg layout-pb-md">
+      <div data-anim-wrap="" class="container animated">
+        <div data-anim-child="slide-up delay-1" class="row justify-center text-center is-in-view">
+          <div class="col-auto">
+            <div class="sectionTitle -md">
+              <h2 class="sectionTitle__title">Lợi ích thuê xe tại Đi Chung</h2>
+            </div>
+          </div>
+        </div>
+
+        <div class="row y-gap-40 justify-between pt-40 sm:pt-20">
+
+          <div data-anim-child="slide-up delay-2" class="col-lg-4 col-sm-6 is-in-view">
+
+            <div class="featureIcon -type-1 -hover-shadow px-50 py-50 lg:px-24 lg:py-15">
+              <div class="d-flex justify-center">
+                <img src="img/featureIcons/2/1.svg" alt="image" class="js-lazy loaded" data-ll-status="loaded">
+              </div>
+
+              <div class="text-center mt-30">
+                <h4 class="text-18 fw-500">Giá tốt nhất</h4>
+                <p class="text-15 mt-10">Chọc lọc giá từ nhiều nhà xe, báo giá chính xác đến từng km. Có xuất hóa đơn đỏ</p>
+              </div>
+            </div>
+
+          </div>
+
+          <div data-anim-child="slide-up delay-3" class="col-lg-4 col-sm-6 is-in-view">
+
+            <div class="featureIcon -type-1 -hover-shadow px-50 py-50 lg:px-24 lg:py-15">
+              <div class="d-flex justify-center">
+                <img src="img/featureIcons/2/2.svg" alt="image" class="js-lazy loaded" data-ll-status="loaded">
+              </div>
+
+              <div class="text-center mt-30">
+                <h4 class="text-18 fw-500">Thoải mái như xe nhà</h4>
+                <p class="text-15 mt-10">Hoàn toàn chủ động lịch trình: xe đi trong ngày, 2 ngày 1 đêm, đưa đón tham quan,... Xe đời mới</p>
+              </div>
+            </div>
+
+          </div>
+
+          <div data-anim-child="slide-up delay-4" class="col-lg-4 col-sm-6 is-in-view">
+
+            <div class="featureIcon -type-1 -hover-shadow px-50 py-50 lg:px-24 lg:py-15">
+              <div class="d-flex justify-center">
+                <img src="img/featureIcons/2/3.svg" alt="image" class="js-lazy loaded" data-ll-status="loaded">
+              </div>
+
+              <div class="text-center mt-30">
+                <h4 class="text-18 fw-500">Bác tài chuyên nghiệp</h4>
+                <p class="text-15 mt-10">Lái xe chuyên tuyến, rành đường. Đưa đón cẩn thận, đúng giờ, chu đáo suốt hành trình</p>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </section>
 
 
     <!-- Email -->
