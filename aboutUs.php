435f9df3ae0f90471bad03b8c5aa23491acc6323
Nhân Vượng <52657226+vuonggluon@users.noreply.github.com>
2023-08-12 09:35:58 +0700

    update layout
    breadcrumb +

--- aboutUs.php
+++ aboutUs.php
@@ -4,33 +4,12 @@
 <!-- Head -->
 
 <body>
-    <!-- Nav -->
-    <?php include 'commons/nav.php';?>
-
-    <!-- Body -->
-    <div class="header-margin"></div>
-    <section class="py-10 d-flex items-center bg-light-2">
-        <div class="container">
-            <div class="row y-gap-10 items-center justify-between">
-                <div class="col-auto">
-                    <div class="row x-gap-10 y-gap-5 items-center text-14 text-light-1">
-                        <div class="col-auto">
-                            <div class="">Trang chủ</div>
-                        </div>
-                        <div class="col-auto">
-                            <div class="">&gt;</div>
-                        </div>
-                        <div class="col-auto">
-                            <div class="">Giới Thiệu</div>
-                        </div>
-                    </div> 
-                </div>
-
-
+  <!-- Nav -->
+  <?php include 'commons/nav.php'; ?>
   <!-- Body -->
   <div class="header-margin"></div>
   <!-- Breadcrumb -->
-  <section data-anim="fade" class="d-flex items-center py-15 border-top-light is-in-view">
+  <!-- <section data-anim="fade" class="d-flex items-center py-15 border-top-light is-in-view">
     <div class="container">
       <div class="row y-gap-10 items-center justify-between">
         <div class="col-auto">
@@ -44,64 +23,7 @@
             <div class="col-auto">
               <div class="text-dark-1"><a href="#">Giới thiệu</a></div>
             </div>
-        </div>
-    </section>
-   
-    <section data-anim="slide-up" class="layout-pt-md layout-pb-md is-in-view">
-      <div class="container">
-      <div class="row y-gap-40 justify-center text-center">
-          <div class="col-auto">
-    
           </div>
-        </div>
-        <div class="row y-gap-30 justify-center">
-          <div class="col-xl-8 col-lg-10">
-            <div class="">
-              <h3 class="text-20 fw-500">Giới thiệu</h3>
-                <div class="border-top-light mt-20"></div>
-              <div class="list-disc text-15 mt-30">
-              <div class="rte">
-					<p><strong>Công ty Lữ hành Hanoitourist</strong>&nbsp;là đơn vị trực thuộc Tổng Công ty Du lịch Hà Nội - doanh nghiệp nhà nước có vị thế hàng đầu trong các lĩnh vực: kinh doanh lữ hành quốc tế, khách sạn, nhà hàng, vận chuyển, xuất khẩu lao động…</p><p>Tiền thân là Công ty Du lịch Hà Nội được thành lập từ ngày 25/3/1963, đến ngày 12/ 7/2004, UBND Thành phố Hà Nội ra Quyết định thành lập Tổng công ty Du lịch Hà Nội, thí điểm hoạt động theo mô hình Công ty mẹ - công ty con, trên cơ sở tập hợp một số doanh nghiệp kinh doanh du lịch trên địa bàn thành phố với mục tiêu tập trung xây dựng một Tổng công ty du lịch lớn, có thương hiệu mạnh, hoạt động đa ngành nghề, đa sở hữu, có sức cạnh tranh cao và hội nhập kinh tế quốc tế hiệu quả, phù hợp với yêu cầu phát triển ngành du lịch và nền kinh tế Thủ đô.</p><p>Qua hơn 50 năm xây dựng và phát triển, từ một Công ty Du lịch, đến nay đã trở thành một Tổng Công ty lớn mạnh gồm 38 Công ty thành viên, Công ty liên doanh liên kết với nước ngoài và trong nước, Công ty cổ phần, Đơn vị phụ thuộc... với hơn 4000 CBCNV tham gia hoạt động sản xuất kinh doanh đa ngành nghề:</p><p>- Trực tiếp tổ chức các hoạt động kinh doanh: Kinh doanh lữ hành quốc tế và nội địa, Kinh doanh khách sạn và vận chuyển khách du lịch bằng đường bộ, đường sông và trên Vịnh, Kinh doanh các dịch vụ du lịch và dịch vụ đại lý bán vé máy bay; Đầu tư, liên doanh liên kết với các thành phần kinh tế trong và ngoài nước để phát triển hạ tầng du lịch: Khu du lịch, Khu vui chơi giải trí, Khách sạn, Nhà hàng, Trung tâm thương mại dịch vụ; Kinh doanh dịch vụ: ăn uống, hướng dẫn du lịch, Phiên dịch, thông tin, vui chơi giải trí, cho người nước ngoài thuê nhà, Cho thuê các phương tiện vận tải;</p><p>- Kinh doanh nhà hàng, đồ lưu niệm; Cho thuê văn phòng; Dịch vụ quảng cáo, Tổ chức hội chợ, triển lãm chuyên đề, hội nghị, hội thảo và Dịch vụ xúc tiến du lịch – thương mại;&nbsp;</p><p>- Kinh doanh xuất khẩu lao động và chuyên gia; Tư vấn du học nước ngoài;</p><p>&nbsp;Tổng Công ty Du lịch Hà Nội từ khi thành lập đã rất quan tâm đến việc tổ chức đào tạo, dạy nghề, bồi dưỡng các nghiệp vụ về: du lịch, khách sạn, hướng dẫn viên du lịch, ngoại ngữ, buồng, bàn, bar, lễ tân, giúp việc gia đình, phục vụ cho các nhiệm vụ sản xuất kinh doanh của Tổng Công ty hướng đến sự phát triển bền vững trong tương lai và đáp ứng nhu cầu ngày càng cao của xã hội; Nhằm đa dạng hóa sản phẩm, Tổng Công ty Du lịch Hà Nội còn thực hiện chức năng sản xuất kinh doanh và đầu tư trong các lĩnh vực: Thương mại, Tài chính, Công nghiệp chế biến nông lâm hải sản, xây dựng phát triển nhà và khu đô thị… phục vụ nhiệm vụ phát triển Du lịch và nhiệm vụ phát triển kinh tế xã hội của Thủ đô.</p><p>&nbsp; &nbsp; &nbsp; &nbsp; Ngoài ra, Tổng công ty còn tham gia với các cơ quan chức năng xây dựng quy hoạch và kế hoạch phát triển ngành Du lịch theo định hướng phát triển kinh tế xã hội của Thành phố cũng như của Chính phủ; Lập, quản lý, tổ chức triển khai thực hiện các dự án đầu tư xây dựng cơ sở hạ tầng phát triển du lịch bằng nguồn vốn ngân sách Nhà nước cấp, vốn vay, vốn huy động của Tổng Công ty.</p><p>Trong đó ngành nghề chính là Kinh doanh lữ hành quốc tế và nội địa, Kinh doanh khách sạn, Vận chuyển khách du lịch, Kinh doanh các dịch vụ du lịch, Đầu tư, Liên doanh liên kết với các tổ chức kinh tế trong và ngoài nước để phát triển cơ sở hạ tầng du lịch và dịch vụ du lịch.</p><p>Tổng Công ty Du lịch Hà Nội không ngừng cải tiến chất lượng dịch vụ, nâng cấp cơ sở hạ tầng, phát triển nhiều mô hình sản phẩm mới cũng như tăng cường tuyên truyền - quảng bá - tiếp thị đến khách hàng.</p><p>&nbsp; &nbsp; &nbsp; &nbsp; Nỗ lực hoạt động đã mang lại cho Tổng Công ty nhiều Huân chương và danh hiệu cao quý: Huân ch­ương Lao động hạng Ba (năm 1980; năm 2010 ), Huân chư­ơng Lao động hạng Hai (năm 1985); Huân ch­ương Lao động hạng Nhất (năm 2002), 2 Cờ Luân l­ưu của Chính phủ, 1 Cờ thi đua của Bộ Văn hoá, Thông tin, Thể thao và Du lịch (năm 1990), 4 Cờ thi đua xuất sắc của UBND Thành phố Hà Nội, 4 Cờ thi đua của Tổng cục Du lịch Việt Nam và nhiều bằng khen, danh hiệu khác… Đặc biệt, Thương hiệu mang tên Hanoitourist và địa chỉ trụ sở chính tại 18 Lý Thường Kiệt đã hình thành và có chỗ đứng vững chắc trong tâm trí khách hàng.</p>
-				</div>
-              </div>
-            <div class="border-top-light mt-40"></div>       
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- <section data-anim="slide-up" class="layout-pt-md layout-pb-md is-in-view">
-    <div class="container">
-      <div class="row y-gap-40 justify-center text-center">
-        <div class="col-auto">
-
-        </div>
-      </div>
-      <div class="row y-gap-30 justify-center">
-        <div class="col-xl-8 col-lg-10">
-          <div class="">
-            <h3 class="text-20 fw-500">Giới thiệu</h3>
-            <div class="border-top-light pt-40 mt-40"></div>
-            <div class="list-disc text-15 mt-30">
-              <div class="rte">
-                <p><strong>Công ty Lữ hành Hanoitourist</strong>&nbsp;là đơn vị trực thuộc Tổng Công ty Du lịch Hà Nội - doanh nghiệp nhà nước có vị thế hàng đầu trong các lĩnh vực: kinh doanh lữ hành quốc tế, khách sạn, nhà hàng, vận chuyển, xuất khẩu lao động…</p>
-                <p>Tiền thân là Công ty Du lịch Hà Nội được thành lập từ ngày 25/3/1963, đến ngày 12/ 7/2004, UBND Thành phố Hà Nội ra Quyết định thành lập Tổng công ty Du lịch Hà Nội, thí điểm hoạt động theo mô hình Công ty mẹ - công ty con, trên cơ sở tập hợp một số doanh nghiệp kinh doanh du lịch trên địa bàn thành phố với mục tiêu tập trung xây dựng một Tổng công ty du lịch lớn, có thương hiệu mạnh, hoạt động đa ngành nghề, đa sở hữu, có sức cạnh tranh cao và hội nhập kinh tế quốc tế hiệu quả, phù hợp với yêu cầu phát triển ngành du lịch và nền kinh tế Thủ đô.</p>
-                <p>Qua hơn 50 năm xây dựng và phát triển, từ một Công ty Du lịch, đến nay đã trở thành một Tổng Công ty lớn mạnh gồm 38 Công ty thành viên, Công ty liên doanh liên kết với nước ngoài và trong nước, Công ty cổ phần, Đơn vị phụ thuộc... với hơn 4000 CBCNV tham gia hoạt động sản xuất kinh doanh đa ngành nghề:</p>
-                <p>- Trực tiếp tổ chức các hoạt động kinh doanh: Kinh doanh lữ hành quốc tế và nội địa, Kinh doanh khách sạn và vận chuyển khách du lịch bằng đường bộ, đường sông và trên Vịnh, Kinh doanh các dịch vụ du lịch và dịch vụ đại lý bán vé máy bay; Đầu tư, liên doanh liên kết với các thành phần kinh tế trong và ngoài nước để phát triển hạ tầng du lịch: Khu du lịch, Khu vui chơi giải trí, Khách sạn, Nhà hàng, Trung tâm thương mại dịch vụ; Kinh doanh dịch vụ: ăn uống, hướng dẫn du lịch, Phiên dịch, thông tin, vui chơi giải trí, cho người nước ngoài thuê nhà, Cho thuê các phương tiện vận tải;</p>
-                <p>- Kinh doanh nhà hàng, đồ lưu niệm; Cho thuê văn phòng; Dịch vụ quảng cáo, Tổ chức hội chợ, triển lãm chuyên đề, hội nghị, hội thảo và Dịch vụ xúc tiến du lịch – thương mại;&nbsp;</p>
-                <p>- Kinh doanh xuất khẩu lao động và chuyên gia; Tư vấn du học nước ngoài;</p>
-                <p>&nbsp;Tổng Công ty Du lịch Hà Nội từ khi thành lập đã rất quan tâm đến việc tổ chức đào tạo, dạy nghề, bồi dưỡng các nghiệp vụ về: du lịch, khách sạn, hướng dẫn viên du lịch, ngoại ngữ, buồng, bàn, bar, lễ tân, giúp việc gia đình, phục vụ cho các nhiệm vụ sản xuất kinh doanh của Tổng Công ty hướng đến sự phát triển bền vững trong tương lai và đáp ứng nhu cầu ngày càng cao của xã hội; Nhằm đa dạng hóa sản phẩm, Tổng Công ty Du lịch Hà Nội còn thực hiện chức năng sản xuất kinh doanh và đầu tư trong các lĩnh vực: Thương mại, Tài chính, Công nghiệp chế biến nông lâm hải sản, xây dựng phát triển nhà và khu đô thị… phục vụ nhiệm vụ phát triển Du lịch và nhiệm vụ phát triển kinh tế xã hội của Thủ đô.</p>
-                <p>&nbsp; &nbsp; &nbsp; &nbsp; Ngoài ra, Tổng công ty còn tham gia với các cơ quan chức năng xây dựng quy hoạch và kế hoạch phát triển ngành Du lịch theo định hướng phát triển kinh tế xã hội của Thành phố cũng như của Chính phủ; Lập, quản lý, tổ chức triển khai thực hiện các dự án đầu tư xây dựng cơ sở hạ tầng phát triển du lịch bằng nguồn vốn ngân sách Nhà nước cấp, vốn vay, vốn huy động của Tổng Công ty.</p>
-                <p>Trong đó ngành nghề chính là Kinh doanh lữ hành quốc tế và nội địa, Kinh doanh khách sạn, Vận chuyển khách du lịch, Kinh doanh các dịch vụ du lịch, Đầu tư, Liên doanh liên kết với các tổ chức kinh tế trong và ngoài nước để phát triển cơ sở hạ tầng du lịch và dịch vụ du lịch.</p>
-                <p>Tổng Công ty Du lịch Hà Nội không ngừng cải tiến chất lượng dịch vụ, nâng cấp cơ sở hạ tầng, phát triển nhiều mô hình sản phẩm mới cũng như tăng cường tuyên truyền - quảng bá - tiếp thị đến khách hàng.</p>
-                <p>&nbsp; &nbsp; &nbsp; &nbsp; Nỗ lực hoạt động đã mang lại cho Tổng Công ty nhiều Huân chương và danh hiệu cao quý: Huân ch­ương Lao động hạng Ba (năm 1980; năm 2010 ), Huân chư­ơng Lao động hạng Hai (năm 1985); Huân ch­ương Lao động hạng Nhất (năm 2002), 2 Cờ Luân l­ưu của Chính phủ, 1 Cờ thi đua của Bộ Văn hoá, Thông tin, Thể thao và Du lịch (năm 1990), 4 Cờ thi đua xuất sắc của UBND Thành phố Hà Nội, 4 Cờ thi đua của Tổng cục Du lịch Việt Nam và nhiều bằng khen, danh hiệu khác… Đặc biệt, Thương hiệu mang tên Hanoitourist và địa chỉ trụ sở chính tại 18 Lý Thường Kiệt đã hình thành và có chỗ đứng vững chắc trong tâm trí khách hàng.</p>
-              </div>
-            </div>
-            <div class="border-top-light pt-40 mt-40"></div>
-          </div>
-        </div>
-      </div>
   </section> -->
 
   <!-- Banner -->
@@ -114,6 +36,11 @@
         <div class="col-xl-6 col-lg-8 col-md-10">
           <h1 class="text-60 md:text-25 fw-700 text-white">GiaTrinh</h1>
           <div class="text-white">Người bạn du lịch đồng hành cùng bạn.</div>
+        </div>
+        <div class="row justify-center text-center text-white-1">
+          <div><a href="index.php">Trang chủ</a></div>
+          <div>&gt;</div>
+          <div><a href="#">Giới thiệu</a></div>
         </div>
       </div>
     </div>
