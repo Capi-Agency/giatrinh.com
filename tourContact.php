40ee3c404bfa201435112b50115db5338951e406
Nhân Vượng <52657226+vuonggluon@users.noreply.github.com>
2023-08-12 11:09:28 +0700

    Revert "Merge branch 'dev-tourDetail' into dev"
    This reverts commit aba8f7bb76bcb6a92d223d3d7c9b261d14329788, reversing
    changes made to 01fa7d48646d8e1be58c9b1fc77f530eea845bf7.

--- tourContact.php
+++ tourContact.php
@@ -171,7 +171,8 @@
             <script src="https://unpkg.com/@googlemaps/markerclusterer/dist/index.min.js"></script>
              <script src="js/vendors.js"></script>
             <script src="js/main.js"></script>
-
+    <!-- Email -->
+    <?php include 'commons/email.php';?>
     <!-- Footer -->
     <?php include 'commons/footer.php';?>
 </body>
--- tourExp.php
+++ tourExp.php
@@ -1,16 +1,194 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include 'commons/head.php';?>
+<?php include 'commons/head.php'; ?>
 <!-- Head -->
+
 <body>
     <!-- Nav -->
-    <?php include 'commons/nav.php';?>
+    <?php include 'commons/nav.php'; ?>
 
     <!-- Body -->
+    <div class="header-margin"></div>
+    <section class="py-10 d-flex items-center bg-light-2">
+        <div class="container">
+            <div class="row y-gap-10 items-center justify-between">
+                <div class="col-auto">
+                    <div class="row x-gap-10 y-gap-5 items-center text-14 text-light-1">
+                        <div class="col-auto">
+                            <div class="">Trang chủ</div>
+                        </div>
+                        <div class="col-auto">
+                            <div class="">></div>
+                        </div>
+                        <div class="col-auto">
+                            <div class="">Kinh nghiệm du lịch</div>
+                        </div>
+                    </div>
+                </div>
 
 
+            </div>
+        </div>
+    </section>
 
+    <section class="py-40 js-pin-container">
+        <div class="container">
+            <div class="row y-gap-30 justify-between">
+                <div class="col-xl-9">
+                    <div class="row y-gap-30">
+                        <div class="col-xl-8">
+                            <div class="row y-gap-40 justify-center">
+                                <img class="col-12 rounded-8" src="./img/blog/1.png" alt="">
+                            </div>
+                            <div class="pt-30">
+                                <a href="./tourPost.php" class="text-18 fw-700">5 làng nghề truyền thống nổi tiếng ở miền Bắc</a>
+                                <p>
+                                    Làng gốm Bát Tràng Bát Tràng là điểm đến không hề xa lạ đối với các bạn trẻ ưa thích nét văn hóa truyền thống của làng nghề gốm sứ V...
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-xl-4">
+                            <div class="row justify-center">
+                                <img class="col-12 rounded-8" src="./img/blog/1.png" alt="">
+                            </div>
+                            <div class="sidebar__item pt-10 text-12">
+                                <a class="text-14 fw-700 ">Chinh phục đỉnh núi Tà Chì Nhù, Yên Bái - 'thiên đường nơi hạ giới'</a>
+                                <div class="y-gap-5 border-bottom-light py-10 mt-10">
+                                    <a href="./tourPost.php" class="d-flex items-center justify-between text-dark-1" href="#">
+                                        Du lịch Bắc Giang, tuyệt đối không được bỏ qua 5 điểm đến 'cực hot'
+                                    </a>
+                                </div>
+                                <div class="y-gap-5 border-bottom-light py-10 mt-10">
+                                    <a href="./tourPost.php" class="d-flex items-center justify-between text-dark-1" href="#">
+                                        Du lịch Bắc Giang, tuyệt đối không được bỏ qua 5 điểm đến 'cực hot'
+                                    </a>
+                                </div>
+                                <div class="y-gap-5 border-bottom-light py-10 mt-10">
+                                    <a href="./tourPost.php" class="d-flex items-center justify-between text-dark-1" href="#">
+                                        Du lịch Bắc Giang, tuyệt đối không được bỏ qua 5 điểm đến 'cực hot'
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row y-gap-30 mt-30 pt-30 ">
+
+                        <div class="col-lg-4 col-sm-6">
+
+                            <a href="" class="blogCard -type-1 d-block ">
+                                <div class="blogCard__image">
+                                    <div class="ratio ratio-4:3 rounded-8">
+                                        <img class="img-ratio js-lazy loaded" src="img/blog/grids/1.png" alt="image" data-ll-status="loaded">
+                                    </div>
+                                </div>
+
+                                <div class="pt-20">
+                                    <a class="text-dark-1 text-18 fw-500">5 làng nghề truyền thống nổi tiếng ở miền Bắc</a>
+                                </div>
+                            </a>
+
+                        </div>
+
+                        <div class="col-lg-4 col-sm-6">
+
+                            <a href="" class="blogCard -type-1 d-block ">
+                                <div class="blogCard__image">
+                                    <div class="ratio ratio-4:3 rounded-8">
+                                        <img class="img-ratio js-lazy loaded" src="img/blog/grids/2.png" alt="image" data-ll-status="loaded">
+                                    </div>
+                                </div>
+
+                                <div class="pt-20">
+                                    <a class="text-dark-1 text-18 fw-500">làng nghề truyền thống nổi tiếng ở miền Bắc</a>
+                                </div>
+                            </a>
+
+                        </div>
+
+                        <div class="col-lg-4 col-sm-6">
+
+                            <a href="" class="blogCard -type-1 d-block ">
+                                <div class="blogCard__image">
+                                    <div class="ratio ratio-4:3 rounded-8">
+                                        <img class="img-ratio js-lazy loaded" src="img/blog/grids/3.png" alt="image" data-ll-status="loaded">
+                                    </div>
+                                </div>
+                                <div class="pt-20">
+                                    <a class="text-dark-1 text-18 fw-500">làng nghề truyền thống nổi tiếng ở miền Bắc</a>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3">
+                    <div data="slide-up delay-3" class="sidebar -blog">
+                        <div class="">
+                            <h5 class="text-18 fw-700 mb-10">Danh mục</h5>
+
+                            <div class="y-gap-5">
+
+                                <a href="./tourList.php" class="d-flex items-center justify-between text-dark-1" href="#">
+                                    <span class="text-15 text-dark-1">Tour trong nước</span>
+                                </a>
+
+                                <a href="./tourList.php" class="d-flex items-center justify-between text-dark-1" href="#">
+                                    <span class="text-15 text-dark-1">Tour quốc tế</span>
+                                </a>
+
+                                <a href="./mainLabor.php" class="d-flex items-center justify-between text-dark-1" href="#">
+                                    <span class="text-15 text-dark-1">Xuất khẩu lao động</span>
+                                </a>
+
+                                <a href="./insurance.php" class="d-flex items-center justify-between text-dark-1" href="#">
+                                    <span class="text-15 text-dark-1">Bảo hiểm</span>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="sidebar__item">
+                            <h5 class="text-18 fw-700 mb-10">Bài viết được yêu thích</h5>
+
+                            <div class="row y-gap-20 pt-10">
+
+                                <div class="col-12">
+                                    <div class="d-flex items-center">
+                                        <img class="size-65 rounded-8" src="img/blog/sidebar/1.png" alt="image">
+                                        <div class="ml-15">
+                                            <a class="text-15 lh-15 fw-500">5 làng nghề truyền thống nổi tiếng ở miền Bắc</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-12">
+                                    <div class="d-flex items-center">
+                                        <img class="size-65 rounded-8" src="img/blog/sidebar/2.png" alt="image">
+                                        <div class="ml-15">
+                                            <a class="text-15 lh-15 fw-500">5 làng nghề truyền thống nổi tiếng ở miền Bắc</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-12">
+                                    <div class="d-flex items-center">
+                                        <img class="size-65 rounded-8" src="img/blog/sidebar/3.png" alt="image">
+                                        <div class="ml-15">
+                                            <a class="text-15 lh-15 fw-500">5 làng nghề truyền thống nổi tiếng ở miền Bắc</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Email -->
+    <?php include 'commons/email.php'; ?>
     <!-- Footer -->
-    <?php include 'commons/footer.php';?>
+    <?php include 'commons/footer.php'; ?>
 </body>
+
 </html>
