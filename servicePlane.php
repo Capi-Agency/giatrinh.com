6e7b64c0d3ce36ac8ad046adaf7ba49bb46109bb
nguyen <125894255+glexiv808@users.noreply.github.com>
2023-08-12 14:00:41 +0700

    Update servicePlane.php

--- servicePlane.php
+++ servicePlane.php
@@ -1,138 +1,131 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include 'commons/head.php';?>
+<?php include 'commons/head.php'; ?>
 <!-- Head -->
-<body>
-    <!-- Nav -->
-    <?php include 'commons/nav.php';?>
 
-    <!-- Body -->
-    <div class="header-margin">
-      <section class="section-bg layout-pt-lg layout-pb-lg">
-        <div class="section-bg__item col-12">
-          <img src="img/pages/about/1.png" alt="image">
-        </div>
-        <div class="container">
-          <div class="row justify-center text-center">
-            <div class="col-xl-6 col-lg-8 col-md-10">
-              <h1 class="text-60 md:text-25 fw-700 text-white">GiaTrinh</h1>
-              <div class="text-white">Người bạn du lịch đồng hành cùng bạn.</div>
-            </div>
+<body>
+  <!-- Nav -->
+  <?php include 'commons/nav.php'; ?>
+
+  <!-- Body -->
+  <div class="header-margin">
+    <!-- Banner Header -->
+
+    <section class="section-bg layout-pt-lg layout-pb-lg">
+      <div class="section-bg__item col-12">
+        <img
+          src="https://images.unsplash.com/photo-1507431489734-ef0dbfbf88e1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1472&q=80"
+          alt="image">
+      </div>
+      <div class="container">
+        <div class="row justify-center text-center">
+          <div class="col-xl-6 col-lg-8 col-md-10">
+            <h1 class="text-60 md:text-25 fw-700 text-white">GiaTrinh</h1>
+            <div class="text-white">Người bạn du lịch đồng hành cùng bạn.</div>
+          </div>
+          <div class="d-flex mt-3 justify-center text-center text-white-1">
+            <div><a class="text-white p-2" href="index.php">Trang chủ</a></div>
+            <div class="text-white">></div>
+            <div><a class="text-white p-2" href="#">Giới thiệu</a></div>
           </div>
         </div>
-      </section>
-    </div>
-    <!-- breadcrumb -->
-    <section data-anim="fade" class="d-flex items-center py-15 border-top-light is-in-view">
-        <div class="container">
-            <div class="row y-gap-10 items-center justify-between">
-                <div class="col-auto">
-                    <div class="row x-gap-10 y-gap-5 items-center text-14 text-light-1">
-                        <div class="col-auto">
-                            <div class=""><a href="index.php">Trang chủ</a></div>
-                        </div>
-                        <div class="col-auto">
-                            <div class="">&gt;</div>
-                        </div>
-                        <div class="col-auto">
-                            <div class="text-dark-1"><a href="#">Mua hộ vé máy bay</a></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+      </div>
     </section>
-    
+    <!-- Main -->
     <section class="layout-pt-sm layout-pb-md bg-light-2 col-12">
       <div class="container">
-        <div class="row y-gap-30">
-          <div class="row pt-50 col-12">
+        <div class="row y-gap-30 ">
+          <div class="row pt-50 col-12 ">
             <div class="text-40 justify flex-center">
               <b>Bảng giá vé máy bay Vietjet, Bamboo và Vietnam Airlines</b>
             </div>
             <div>
-              Bảng giá vé máy bay Vietjet, Bamboo và Vietnam Airlines các tháng trong năm với giá tốt nhất. Cần là có, tìm là thấy giá vé tốt nhất ở từng thời điểm. Hãy tranh thủ đặt sớm để có giá tốt.<br>
+              Bảng giá vé máy bay Vietjet, Bamboo và Vietnam Airlines các tháng trong năm với giá tốt nhất. Cần là có,
+              tìm là thấy giá vé tốt nhất ở từng thời điểm. Hãy tranh thủ đặt sớm để có giá tốt.<br>
               Mẹo săn vé máy bay giá rẻ:<br>
-                  Cách đặt vé máy bay trong nước <br>
-                  Bạn truy cập thường xuyên hệ thống đặt vé máy bay trực tuyến, Giatrinh.com Hãy theo dõi fanpage chúng tôi : Gia Trinh. Thông tin liên tục được làm mới từ đội ngũ booker trên trang này. Hotline hỗ trợ trực tuyến 24/7 : zalo, messenger, telegram .. . 098 258 8080. Cần là có, tìm là thấy giá vé tốt nhất.
-                  <ol>
-                    <li>Bạn phải lên kế hoạch cụ thể cho chuyến đi.</li>
-                    <li>Giá vé máy bay rẻ nhất là 1-2 tháng trước khi bay.</li>
-                    <li>Tập thể đồng thuận trước khi tiến hành đặt vé đoàn.</li>
-                    <li>Chúng ta không thực hiện nhiệm vụ bất khả thi : vé cận.</li>
-                    <li>Booker chuyên nghiệp luôn là giải pháp tối ưu trong mọi trường hợp.</li>
-                    <li>Bạn nên gởi mail ghi rõ yêu cầu và ủy thác cho họ xử lý booking.</li>
-                  </ol>
-                </p>
+              Cách đặt vé máy bay trong nước <br>
+              Bạn truy cập thường xuyên hệ thống đặt vé máy bay trực tuyến, Giatrinh.com Hãy theo dõi fanpage chúng tôi
+              : Gia Trinh. Thông tin liên tục được làm mới từ đội ngũ booker trên trang này. Hotline hỗ trợ trực tuyến
+              24/7 : zalo, messenger, telegram .. . 098 258 8080. Cần là có, tìm là thấy giá vé tốt nhất.
+              <ol>
+                <li>Bạn phải lên kế hoạch cụ thể cho chuyến đi.</li>
+                <li>Giá vé máy bay rẻ nhất là 1-2 tháng trước khi bay.</li>
+                <li>Tập thể đồng thuận trước khi tiến hành đặt vé đoàn.</li>
+                <li>Chúng ta không thực hiện nhiệm vụ bất khả thi : vé cận.</li>
+                <li>Booker chuyên nghiệp luôn là giải pháp tối ưu trong mọi trường hợp.</li>
+                <li>Bạn nên gởi mail ghi rõ yêu cầu và ủy thác cho họ xử lý booking.</li>
+              </ol>
+              </p>
             </div>
-                    <div class="col-12 row y-gap-30">
-                      <img src="https://vietjet.net/includes/uploads/2017/07/bang-gia-ve-may-bay-vietjet-air.jpg" alt="image" class="col-12 rounded-8">
-                        <div class="layout-mt-sm "></div>
+            <div class="col-12 row y-gap-30 justify-content-center">
+              <img src="https://vietjet.net/includes/uploads/2017/07/bang-gia-ve-may-bay-vietjet-air.jpg" alt="image"
+                class="col-8 rounded-8">
+              <div class="layout-mt-sm "> </div>
 
-                        <table class="table col-12 ">
-                        
-                          <thead class="bg-blue-1-05 text-blue-1">
-                            <tr>
-                              <th>Chặng bay</th>
-                              <th>Giá vé</th>
-                            </tr>
-                          </thead>
-                          <tbody>
+              <table class="table col-12 ">
 
-                            <tr>
+                <thead class="bg-blue-1-05 text-blue-1">
+                  <tr>
+                    <th>Chặng bay</th>
+                    <th>Giá vé</th>
+                  </tr>
+                </thead>
+                <tbody>
 
-                              <td>Hà Nội - tp Hồ Chí Minh</td>
-                              <td>890.000đ</td>
-              
-                            </tr>
+                  <tr>
 
-                            <tr>
-                              <td>Hà Nội - Đà Nẵng</td>
-                              <td>490.000đ</td>
+                    <td>Hà Nội - tp Hồ Chí Minh</td>
+                    <td>890.000đ</td>
 
-                            </tr>
+                  </tr>
 
+                  <tr>
+                    <td>Hà Nội - Đà Nẵng</td>
+                    <td>490.000đ</td>
 
-                            <tr>
-                              <td>Hà Nội - Phú Quốc</td>
-                              <td>1.000.000đ</td>
-                              
-                            </tr>
-                            <tr>
-                              <td>Hà Nội - tp Hồ Chí Minh</td>
-                              <td>890.000đ</td>
-                              
-                            </tr>
-                            <tr>
-                              <td>Hà Nội - Đà Lạt</td>
-                              <td>900.000đ</td>
-                              
-                            </tr>
-                            <tr>
-                              <td>Hà Nội - tp Hồ Chí Minh</td>
-                              <td>890.000đ</td>
-                              
-                            </tr>
-                            <tr>
-                              <td>Hà Nội - tp Hồ Chí Minh</td>
-                              <td>890.000đ</td>
-                              
-                            </tr>
-                            <tr>
-                              <td>Hà Nội - tp Hồ Chí Minh</td>
-                              <td>890.000đ</td>
-                              
-                            </tr>
-                            <tr>
-                              <td>Hà Nội - tp Hồ Chí Minh</td>
-                              <td>890.000đ</td>
-                              
-                            </tr>
+                  </tr>
 
 
-                          </tbody>
-                        </table>
-                    </div>
+                  <tr>
+                    <td>Hà Nội - Phú Quốc</td>
+                    <td>1.000.000đ</td>
+
+                  </tr>
+                  <tr>
+                    <td>Hà Nội - tp Hồ Chí Minh</td>
+                    <td>890.000đ</td>
+
+                  </tr>
+                  <tr>
+                    <td>Hà Nội - Đà Lạt</td>
+                    <td>900.000đ</td>
+
+                  </tr>
+                  <tr>
+                    <td>Hà Nội - tp Hồ Chí Minh</td>
+                    <td>890.000đ</td>
+
+                  </tr>
+                  <tr>
+                    <td>Hà Nội - tp Hồ Chí Minh</td>
+                    <td>890.000đ</td>
+
+                  </tr>
+                  <tr>
+                    <td>Hà Nội - tp Hồ Chí Minh</td>
+                    <td>890.000đ</td>
+
+                  </tr>
+                  <tr>
+                    <td>Hà Nội - tp Hồ Chí Minh</td>
+                    <td>890.000đ</td>
+
+                  </tr>
+
+
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
@@ -159,7 +152,8 @@
 
               <div class="text-center mt-30">
                 <h4 class="text-18 fw-500">Giá tốt nhất</h4>
-                <p class="text-15 mt-10">Chọc lọc giá từ nhiều đối tác, báo giá chính xác đến từng địa điểm. Có xuất hóa đơn đỏ</p>
+                <p class="text-15 mt-10">Chọc lọc giá từ nhiều đối tác, báo giá chính xác đến từng địa điểm, có xuất hóa
+                  đơn đỏ</p>
               </div>
             </div>
 
@@ -200,8 +194,9 @@
     </section>
 
     <!-- Email -->
-    <?php include 'commons/email.php';?>
+    <?php include 'commons/email.php'; ?>
     <!-- Footer -->
-    <?php include 'commons/footer.php';?>
+    <?php include 'commons/footer.php'; ?>
 </body>
+
 </html>
