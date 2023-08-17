63f6f9a7c71b6d753990a2bd12043b17e83c8fbf
ducnguyen1002 <133721410+ducnguyen1002@users.noreply.github.com>
2023-08-17 09:26:01 +0700

    fix header render theo data nhận về (#47)

--- commons/header.php
+++ commons/header.php
@@ -1,18 +1,19 @@
 <section class="section-bg layout-pt-md layout-pb-md">
-    <div class="section-bg__item col-12">
-      <img src="https://images.unsplash.com/photo-1507431489734-ef0dbfbf88e1?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=1472&amp;q=80" alt="image">
-    </div>
-    <div class="container">
-      <div class="row justify-center text-center">
-        <div class="col-xl-6 col-lg-8 col-md-10">
-          <h1 class="text-60 md:text-25 fw-700 text-white">GiaTrinh</h1>
-          <div class="text-white">Người bạn du lịch đồng hành cùng bạn.</div>
-        </div>
-        <div class="d-flex mt-3 justify-center text-center text-white-1">
-          <div><a class="text-white p-2" href="index.php">Trang chủ</a></div>
-          <div class="text-white">&gt;</div>
-          <div><a class="text-white p-2" href="#">(Trang)</a></div>
-        </div>
+  <div class="section-bg__item col-12">
+    <img id="header_img" alt="header img" class="js-lazy loaded" data-ll-status="loaded" />
+    <!-- <img src="https://images.unsplash.com/photo-1507431489734-ef0dbfbf88e1?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=1472&amp;q=80" alt="image"> -->
+  </div>
+  <div class="container">
+    <div class="row justify-center text-center">
+      <div class="col-xl-6 col-lg-8 col-md-10">
+        <h1 class="text-60 md:text-25 fw-700 text-white">GiaTrinh</h1>
+        <div class="text-white">Người bạn du lịch đồng hành cùng bạn.</div>
+      </div>
+      <div class="d-flex mt-3 justify-center text-center text-white-1">
+        <div><a class="text-white p-2" href="index.php">Trang chủ</a></div>
+        <div class="text-white">&gt;</div>
+        <div> <a class="text-white p-2" href="" id="breadcrumb_anchor"></a></div>
       </div>
     </div>
-  </section>
+  </div>
+</section>
--- serviceCar.php
+++ serviceCar.php
@@ -10,24 +10,7 @@
   <!-- Body -->
   <div class="header-margin"></div>
   <!-- Banner Header -->
-  <section class="section-bg layout-pt-lg layout-pb-lg">
-    <div class="section-bg__item col-12">
-      <img src="https://images.unsplash.com/photo-1507431489734-ef0dbfbf88e1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1472&q=80" alt="image">
-    </div>
-    <div class="container">
-      <div class="row justify-center text-center">
-        <div class="col-xl-6 col-lg-8 col-md-10">
-          <h1 class="text-60 md:text-25 fw-700 text-white">GiaTrinh</h1>
-          <div class="text-white">Người bạn du lịch đồng hành cùng bạn.</div>
-        </div>
-        <div class="d-flex mt-3 justify-center text-center text-white-1">
-          <div><a class="text-white p-2" href="index.php">Trang chủ</a></div>
-          <div class="text-white">&gt;</div>
-          <div><a class="text-white p-2" href="#">Dịch vụ thuê xe</a></div>
-        </div>
-      </div>
-    </div>
-  </section>
+  <?php include 'commons/header.php'; ?>
   <!-- Bảng 1 -->
   <section class="layout-pt-md layout-pb-md">
     <div class="container">
