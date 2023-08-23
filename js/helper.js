10a69479df00d2c0ab34cb1b9dfadddc46b93003
ducnguyen1002 <133721410+ducnguyen1002@users.noreply.github.com>
2023-08-23 15:50:07 +0700

    Fix index search bar (#56)
    * tìm kiếm tour theo địa điểm + thời gian
    
    * update theme flatpickr
    
    * update main.css
    
    * Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -114,7 +114,8 @@ function getSearchValueAndNavigate(locations) {
       urlParams.push(locationParam);
     }
     if (datePickerInput.value || datePickerInput.value != "") {
-      const [startDate, endDate] = datePickerInput.value.split(" to ");
+      const [startDate, endDate] = datePickerInput.value.split(" đến ");
+
       let durationParam = `duration=${calcDuration(startDate, endDate)}`;
       urlParams.push(durationParam);
     }
